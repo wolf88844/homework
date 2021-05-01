7eefb4843a5597c91130de292180d1cd9f60561b
wolf88844 <wolf88840@163.com>
2021-05-01 11:46:41 +0800

    第三周作业

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module homework
 go 1.16
 
 require (
+	github.com/da440dil/go-workgroup v0.0.5
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/pkg/errors v0.9.1
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
---
+++ homework3/homework3.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type httpSrv struct {
+	*http.Server
+	ctx context.Context
+}
+
+func (h *httpSrv) Start(address string) error {
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
+	}
+	err = h.Serve(lis)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *httpSrv) Stop() error {
+	return h.Shutdown(h.ctx)
+}
+
+func newServer(mux *http.ServeMux, ctx context.Context) *httpSrv {
+	debugHttp := httpSrv{Server: &http.Server{Handler: mux}, ctx: ctx}
+	return &debugHttp
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	g, errCtx := errgroup.WithContext(ctx)
+
+	debugMux := http.NewServeMux()
+	debugMux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "debug goruntine")
+	})
+	debugHttp := newServer(debugMux, errCtx)
+	g.Go(func() error {
+		return debugHttp.Start("127.0.0.1:8080")
+	})
+	g.Go(func() error {
+		<-errCtx.Done()
+		fmt.Println("debug 退出")
+		return debugHttp.Stop()
+	})
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		select {
+		case <-time.After(1 * time.Second):
+			fmt.Fprintf(writer, "时间到期")
+			cancel()
+		}
+	})
+	muxHttp := newServer(mux, errCtx)
+	g.Go(func() error {
+		return muxHttp.Start("127.0.0.1:8081")
+	})
+	g.Go(func() error {
+		<-errCtx.Done()
+		fmt.Println("mux 退出")
+		return muxHttp.Stop()
+	})
+
+	//命令
+	signals := []os.Signal{syscall.SIGTERM, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, signals...)
+	g.Go(func() error {
+		for {
+			select {
+			case <-errCtx.Done():
+				return errCtx.Err()
+			case <-c:
+				cancel()
+			}
+		}
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Printf("退出 %+v", err)
+	}
+}
